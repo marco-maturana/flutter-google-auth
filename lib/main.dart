3d526cef6e7ed11abf8ce8d9ceb4eb37ff9ba836
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2024-09-10 19:38:34 -0300

    add flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter_google_auth/ui/app.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
+
   runApp(
     const ProviderScope(
       child: App(),
