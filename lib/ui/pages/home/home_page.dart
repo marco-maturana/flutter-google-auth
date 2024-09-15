c47414f4faddffe90fcf6e18b1dd60548aba4c9a
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2024-09-15 16:37:12 -0300

    signOut button config in the home page

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'package:flutter_google_auth/domains/auth/providers/auth.dart';
 import 'package:flutter_google_auth/ui/templates/default/default_template.dart';
 
-class HomePage extends HookWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultTemplate(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -20,7 +20,8 @@ class HomePage extends HookWidget {
             ),
           ),
           ElevatedButton(
-            onPressed: () => context.go('/auth'),
+            onPressed: () async =>
+                await ref.read(authProvider.notifier).signOut(),
             style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(3.0),
@@ -32,7 +33,7 @@ class HomePage extends HookWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  'Go to AuthPage',
+                  'Sign Out',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
