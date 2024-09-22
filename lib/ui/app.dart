8c687b9e94aab3f1f8cc4efe409f8877aae7a1ae
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2024-09-22 20:16:50 -0300

    app widget using HookConsumerWidget

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'router/router_provider.dart';
 
-class App extends StatelessWidget {
+class App extends HookConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(builder: (_, ref, __) {
-      final router = ref.watch(routerProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
 
-      return MaterialApp.router(
-        title: 'Flutter Google Auth',
-        debugShowCheckedModeBanner: false,
-        routerConfig: router,
-      );
-    });
+    return MaterialApp.router(
+      title: 'Flutter Google Auth',
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
   }
 }
